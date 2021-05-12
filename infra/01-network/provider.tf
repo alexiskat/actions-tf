92100145495c66de25e9f3a4aad12e8653a6258b
alexis <46929771+weebaws@users.noreply.github.com>
2021-05-12 21:24:22 +0100

    fmt

--- infra/01-network/provider.tf
+++ infra/01-network/provider.tf
@@ -1,14 +1,14 @@
 
 terraform {
-    required_providers {
-        hashiaws = {
-            source = "hashicorp/aws"
-            version = "~> 3.32.0"
-        }
+  required_providers {
+    hashiaws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.32.0"
     }
-    required_version = ">= 0.13"
+  }
+  required_version = ">= 0.13"
 }
 
-provider aws {
-    region = "eu-west-1"
+provider "aws" {
+  region = "eu-west-1"
 }
